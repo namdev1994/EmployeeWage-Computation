d9d25d580d918d284690db63fad51b436ce03e8f
namdev1994 <n.lavhare1994@gmail.com>
2021-04-26 22:26:23 +0530

    Added:EmpWage Calculation Using Switch Case

--- src/empWagebuider/EmpWageBuilder.java
+++ src/empWagebuider/EmpWageBuilder.java
@@ -5,35 +5,31 @@ public class EmpWageBuilder {
 	public static void main(String ards[])
 	{
 		System.out.println("Welcome to EmployeeWage Computation");
+		int income=0;
 		int Is_Full_Time=1;
 		int Is_Part_Part=0;
 		int EmpRate_PerHr=20;
 		int FullDay_Hr=8;
 		int PartTime_Hr=4;
 		Random rand=new Random();
-		int n=rand.nextInt(2);
-		Random randX=new Random();
-		int m=randX.nextInt(2);
-		if(n==1)
+		int n=rand.nextInt(3);
+		System.out.println(n);
+		switch(n)
 		{
-			if(m==Is_Full_Time)
-			{
-				
-			int income=EmpRate_PerHr*FullDay_Hr;
-			System.out.println("Employee Is Present And Full Day Earning Is="+income);
-			}
-			else
-			{
-				int income=EmpRate_PerHr*PartTime_Hr;
-				System.out.println("Employee Is Present And Part Time Day Earning Is="+income);
-			}
+		case 1:
+			income=EmpRate_PerHr*FullDay_Hr;
+			System.out.println("Employee Is Present And Full Day Earning Is ="+income);
+			break;
+		case 2:
+			 income=EmpRate_PerHr*PartTime_Hr;
+			System.out.println("Employee Is Present And Part Time Day Earning Is ="+income);
+			break;
+		default:
+			System.out.println("Employee is Absent And Earning Is ="+income);
+			break;
 			
 		}
-		else
-		{
-			System.out.println("Employee is Absent");
-		}
-		
-	}
+			
 
+}
 }
