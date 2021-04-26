2f3604738800408358fb61d32d1bef27e7cbef30
namdev1994 <n.lavhare1994@gmail.com>
2021-04-26 20:12:33 +0530

    added:Employee Attendance check

--- src/empWagebuider/EmpWageBuilder.java
+++ src/empWagebuider/EmpWageBuilder.java
@@ -1,9 +1,22 @@
 package empWagebuider;
+import java.util.*;
 
 public class EmpWageBuilder {
 	public static void main(String ards[])
 	{
 		System.out.println("Welcome to EmployeeWage Computation");
+		Random rand=new Random();
+		int n=rand.nextInt(2);
+		System.out.println(n);
+		if(n==1)
+		{
+			System.out.println("Employee is present");
+			
+		}
+		else
+		{
+			System.out.println("Employee is Absent");
+		}
 		
 	}
 
