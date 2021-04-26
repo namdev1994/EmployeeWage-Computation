d1c272ba2e3f4bc3b15e120d2efd14aec5961a20
namdev1994 <n.lavhare1994@gmail.com>
2021-04-26 21:34:27 +0530

    Added :EmployeeWage calculation for full day

--- src/empWagebuider/EmpWageBuilder.java
+++ src/empWagebuider/EmpWageBuilder.java
@@ -5,12 +5,15 @@ public class EmpWageBuilder {
 	public static void main(String ards[])
 	{
 		System.out.println("Welcome to EmployeeWage Computation");
+		int EmpRate_PerHr=20;
+		int FullDay_Hr=8;
 		Random rand=new Random();
 		int n=rand.nextInt(2);
 		System.out.println(n);
 		if(n==1)
 		{
-			System.out.println("Employee is present");
+			int income=EmpRate_PerHr*FullDay_Hr;
+			System.out.println("Employee is present And Full Earning is="+income);
 			
 		}
 		else
